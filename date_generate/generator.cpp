1d1a96c4a442d8086377140851e8ca2244e54620
AlanXue1997 <836106158@qq.com>
2018-05-19 14:04:56 +0800

    三种搜索全部完成

--- date_generate/generator.cpp
+++ date_generate/generator.cpp
@@ -6,8 +6,17 @@
 #define MAX_LENGTH 300
 #define SWAP(x,y) x^=y^=x^=y
 
-#define LINK_INDEX
-#define BINARY_INDEX
+//#define LINK_INDEX
+//#define BINARY_INDEX
+#define B_TREE_INDEX
+
+int tree_index = 0x00CCCC00;
+
+typedef struct RET {
+	int min;
+	int index;
+	unsigned char *blk;
+}ret;
 
 void buble_sort(int *a, int *b, int len) {
 	for (int i = 0; i < len; ++i) {
@@ -17,6 +26,64 @@ void buble_sort(int *a, int *b, int len) {
 				SWAP(b[i], b[j]);
 			}
 		}
+	}
+}
+
+ret build_tree(int *a, int *b, int len, Buffer &buf) {
+	unsigned char* blk;
+	int *arr;
+	if (len <= 7) {
+		blk = getNewBlockInBuffer(&buf);
+		arr = (int*)blk;
+		arr[0] = 0xFFFFFFFF;
+		for (int i = 0; i < len; ++i) {
+			arr[i * 2 + 2] = a[i];
+			arr[i * 2 + 1] = b[i];
+		}
+		arr[15] = 0;
+		return ret{ arr[2], tree_index++, blk };
+	}
+	else {
+		int n = 8;
+		blk = getNewBlockInBuffer(&buf);
+		arr = (int*)blk;
+		arr[0] = 0;
+		ret ans, r;
+		int k = 0;
+		unsigned char *last_blk = NULL;
+		int last_index = 0;
+		while (n > 0) {
+			int l = len / n;
+			r = build_tree(a, b, l, buf);
+			int *temp = (int*)r.blk;
+			if (last_blk != NULL) {
+				if (temp[0] != 0) {
+					*(((int*)last_blk) + 15) = r.index;
+				}
+				writeBlockToDisk(last_blk, last_index, &buf);
+				freeBlockInBuffer(last_blk, &buf);
+			}
+			last_blk = r.blk;
+			last_index = r.index;
+			if (k == 0) {
+				ans.min = r.min;
+				arr[1] = r.index;
+			}
+			else {
+				arr[k * 2] = r.min;
+				arr[k * 2 + 1] = r.index;
+			}
+			a += l;
+			b += l;
+			len -= l;
+			n--;
+			k++;
+		}
+		writeBlockToDisk(r.blk, r.index, &buf);
+		freeBlockInBuffer(r.blk, &buf);
+		ans.index = tree_index++;
+		ans.blk = blk;
+		return ans;
 	}
 }
 
@@ -36,6 +103,13 @@ int main() {
 	int *arr = NULL;
 	int a[MAX_LENGTH];
 	int b[MAX_LENGTH];
+#ifdef B_TREE_INDEX
+	//建立B树需要的变量
+	int indexes[MAX_LENGTH];
+	int values[MAX_LENGTH];
+	int b_tree_flag;
+	ret root;
+#endif
 	for (int i = 0; i < r_length; ++i) {
 		a[i] = rand() % 40 + 1;
 		b[i] = rand() % 1000 + 1;
@@ -48,6 +122,9 @@ int main() {
 #endif
 #ifdef BINARY_INDEX
 	FILE *r_binary_index = fopen("../data/r_binary.index", "w");
+#endif
+#ifdef B_TREE_INDEX
+	b_tree_flag = 0;
 #endif
 	for (int i = 0; i < r_length; ++i) {
 		//这时需要新建一个block
@@ -62,13 +139,25 @@ int main() {
 #ifdef BINARY_INDEX
 			fprintf(r_binary_index, "%d %d\n", a[i - 6], block_index);
 #endif
-			*arr = block_index + 1;
+#ifdef B_TREE_INDEX
+			values[b_tree_flag] = a[i - 6];
+			indexes[b_tree_flag++] = block_index;
+#endif
+			*arr = i==r_length-1?0:block_index + 1;
 			writeBlockToDisk(blk, block_index++, &buf);
 			freeBlockInBuffer(blk, &buf);
 		}
 	}
 #ifdef BINARY_INDEX
 	fclose(r_binary_index);
+#endif
+#ifdef B_TREE_INDEX
+	root = build_tree(values, indexes, b_tree_flag, buf);
+	writeBlockToDisk(root.blk, root.index, &buf);
+	freeBlockInBuffer(root.blk, &buf);
+	FILE *r_tree_index = fopen("../data/r_tree.index", "w");
+	fprintf(r_tree_index, "%d\n", root.index);
+	fclose(r_tree_index);
 #endif
 
 	for (int i = 0; i < s_length; ++i) {
@@ -84,6 +173,9 @@ int main() {
 #ifdef BINARY_INDEX
 	FILE *s_binary_index = fopen("../data/s_binary.index", "w");
 #endif
+#ifdef B_TREE_INDEX
+	b_tree_flag = 0;
+#endif
 	for (int i = 0; i < s_length; ++i) {
 		//这时需要新建一个block
 		if (i % 7 == 0) {
@@ -97,12 +189,24 @@ int main() {
 #ifdef BINARY_INDEX
 			fprintf(s_binary_index, "%d %d\n", a[i - 6], block_index);
 #endif
-			*arr = block_index + 1;
+#ifdef B_TREE_INDEX
+			values[b_tree_flag] = a[i - 6];
+			indexes[b_tree_flag++] = block_index;
+#endif
+			*arr = i == s_length - 1 ? 0 : block_index + 1;
 			writeBlockToDisk(blk, block_index++, &buf);
 			freeBlockInBuffer(blk, &buf);
 		}
 	}
 #ifdef BINARY_INDEX
 	fclose(s_binary_index);
+#endif
+#ifdef B_TREE_INDEX
+	root = build_tree(values, indexes, b_tree_flag, buf);
+	writeBlockToDisk(root.blk, root.index, &buf);
+	freeBlockInBuffer(root.blk, &buf);
+	FILE *s_tree_index = fopen("../data/s_tree.index", "w");
+	fprintf(s_tree_index, "%d\n", root.index);
+	fclose(s_tree_index);
 #endif
 }
--- project/test.cpp
+++ project/test.cpp
@@ -22,7 +22,176 @@ void link_search(Buffer &buf) {
 		p = arr[2 * 7];
 		freeBlockInBuffer(blk, &buf);
 	}
+
+	fprintf(link_search_output, "\n");
+
+	FILE *s_link_index = fopen("../data/s_link.index", "r");
+	fscanf(s_link_index, "%d", &p);
+	fclose(s_link_index);
+	while ((blk = readBlockFromDisk(p, &buf)) != NULL)
+	{
+		int *arr = (int*)blk;
+		for (int i = 0; i < 7; ++i) {
+			if (arr[i * 2] == 60) {
+				fprintf(link_search_output, "%d %d\n", arr[i * 2], arr[i * 2 + 1]);
+			}
+		}
+		p = arr[2 * 7];
+		freeBlockInBuffer(blk, &buf);
+	}
+
 	fclose(link_search_output);
+}
+
+void binary_search(Buffer &buf) {
+	FILE *binary_search_output = fopen("binary_search.txt", "w");
+	int p[100];
+	int v[100];
+	int len, left, right, middle;
+	unsigned char *blk;
+
+	FILE *r_link_index = fopen("../data/r_binary.index", "r");
+	len = 0;
+	while(fscanf(r_link_index, "%d%d", &v[len], &p[len]) == 2)len++;
+	len++;
+	fclose(r_link_index);
+	left = 0;
+	right = len - 1;
+	middle = (left + right) / 2;
+	while (left < right - 1) {
+		if (v[middle] < 40) {
+			left = middle;
+		}
+		else {
+			right = middle;
+		}
+		middle = (left + right) / 2;
+	}
+	while (v[right] == 40)right++;
+	while (left < right && (blk = readBlockFromDisk(p[left], &buf)) != NULL)
+	{
+		int *arr = (int*)blk;
+		for (int i = 0; i < 7; ++i) {
+			if (arr[i * 2] == 40) {
+				fprintf(binary_search_output, "%d %d\n", arr[i * 2], arr[i * 2 + 1]);
+			}
+		}
+		freeBlockInBuffer(blk, &buf);
+		left++;
+	}
+
+	fprintf(binary_search_output, "\n");
+
+	FILE *s_link_index = fopen("../data/s_binary.index", "r");
+	len = 0;
+	while (fscanf(s_link_index, "%d%d", &v[len], &p[len])==2)len++;
+	len++;
+	fclose(s_link_index);
+	left = 0;
+	right = len - 1;
+	middle = (left + right) / 2;
+	while (left < right - 1) {
+		if (v[middle] < 60) {
+			left = middle;
+		}
+		else {
+			right = middle;
+		}
+		middle = (left + right) / 2;
+	}
+	while (v[right] == 60)right++;
+	while (left < right && (blk = readBlockFromDisk(p[left], &buf)) != NULL)
+	{
+		int *arr = (int*)blk;
+		for (int i = 0; i < 7; ++i) {
+			if (arr[i * 2] == 60) {
+				fprintf(binary_search_output, "%d %d\n", arr[i * 2], arr[i * 2 + 1]);
+			}
+		}
+		freeBlockInBuffer(blk, &buf);
+		left++;
+	}
+
+	fclose(binary_search_output);
+}
+
+void tree_search(Buffer &buf) {
+	FILE *tree_search_output = fopen("tree_search.txt", "w");
+	int p;
+	unsigned char *blk;
+	int *arr;
+
+	FILE *r_tree_index = fopen("../data/r_tree.index", "r");
+	fscanf(r_tree_index, "%d", &p);
+	fclose(r_tree_index);
+	blk = readBlockFromDisk(p, &buf);
+	arr = (int*)blk;
+	while (arr[0] == 0) {
+		int k = 0;
+		while (k < 7 && arr[k * 2 + 2] < 40)k++;
+		p = arr[k * 2 - 1];
+		freeBlockInBuffer(blk, &buf);
+		blk = readBlockFromDisk(p, &buf);
+		arr = (int*)blk;
+	}
+
+	do {
+		p = arr[15];
+		int flag = 0;
+		while (flag < 7 && arr[flag * 2 + 2] <= 40) {
+			unsigned char *sub_blk = readBlockFromDisk(arr[flag * 2 + 1], &buf);
+			if (sub_blk == NULL)break;
+			int *sub_arr = (int*)sub_blk;
+			for (int i = 0; i < 7; ++i) {
+				if (sub_arr[i * 2] == 40) {
+					fprintf(tree_search_output, "%d %d\n", sub_arr[i * 2], sub_arr[i * 2 + 1]);
+				}
+			}
+			freeBlockInBuffer(sub_blk, &buf);
+			flag++;
+		}
+		freeBlockInBuffer(blk, &buf);
+		blk = readBlockFromDisk(p, &buf);
+		arr = (int*)blk;
+	} while (blk != NULL);
+
+	fprintf(tree_search_output, "\n");
+
+	FILE *s_tree_index = fopen("../data/s_tree.index", "r");
+	fscanf(s_tree_index, "%d", &p);
+	fclose(s_tree_index);
+	blk = readBlockFromDisk(p, &buf);
+	arr = (int*)blk;
+	while (arr[0] == 0) {
+		int k = 0;
+		while (k < 7 && arr[k * 2 + 2] < 60)k++;
+		p = arr[k * 2 - 1];
+		freeBlockInBuffer(blk, &buf);
+		blk = readBlockFromDisk(p, &buf);
+		arr = (int*)blk;
+	}
+
+	do {
+		p = arr[15];
+		int flag = 0;
+		while (flag < 7 && arr[flag * 2 + 2] <= 60) {
+			unsigned char *sub_blk = readBlockFromDisk(arr[flag * 2 + 1], &buf);
+			if (sub_blk == NULL)break;
+			int *sub_arr = (int*)sub_blk;
+			for (int i = 0; i < 7; ++i) {
+				if (sub_arr[i * 2] == 60) {
+					fprintf(tree_search_output, "%d %d\n", sub_arr[i * 2], sub_arr[i * 2 + 1]);
+				}
+			}
+			freeBlockInBuffer(sub_blk, &buf);
+			flag++;
+		}
+		freeBlockInBuffer(blk, &buf);
+		blk = readBlockFromDisk(p, &buf);
+		arr = (int*)blk;
+	} while (blk != NULL);
+
+	fclose(tree_search_output);
 }
 
 int main(int argc, char **argv)
@@ -35,7 +204,9 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	link_search(buf);
+	//link_search(buf);
+	//binary_search(buf);
+	tree_search(buf);
 
 	return 0;
 }
